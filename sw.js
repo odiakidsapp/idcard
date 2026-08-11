4aa4eb49defd11cc0e091b16967b47f51663f575
odiakidsapp <odiakidsapp@gmail.com>
2026-08-12 00:31:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-const CACHE_NAME = 'student-data-v37-auto-load-update'; // Bumped to v35
+const CACHE_NAME = 'student-data-v38-auto-load-update'; // Bumped to v35
 
 const urlsToCache = [
   './',
